403a4d8e9190bf72b655e25a5ec7a8d30dbbe52e
Vlad Shapovalov <118466422+Proxym0@users.noreply.github.com>
2023-08-21 21:53:34 +0300

    Added required dependencies. Liquibase Migration is not connected.(21.08.2023, 21.53)

---
+++ src/main/java/com/example/buildingstandem/BuildingsTandemApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildingstandem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingsTandemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingsTandemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buildingstandem/BuildingsTandemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildingstandem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingsTandemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
